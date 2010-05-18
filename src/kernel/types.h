2e4f3725da2faa2df8d160ce195ce1b7ff7d1730
Matthieu Maury <mama5321@spjutet.it.uu.se>
2010-05-18 03:46:53 +0200

    Modify .gitignore (untrack *~)

--- src/kernel/types.h
+++ src/kernel/types.h
@@ -1,19 +1,17 @@
 /**
  * @file types.h
  * @brief Defines types use in the kernel
- */
-
+ */  
+  
 #ifndef TYPES_H
 #define TYPES_H
-
-typedef unsigned char  uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int   uint32_t;
-
-typedef signed char  int8_t;
-typedef signed short int16_t;
-typedef signed int   int32_t;
-
-#endif
-
-/* end of file types.h */
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+
+#endif /*  */
+  
+/* end of file types.h */ 
