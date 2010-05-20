2819f0249445bc05233ce520be449021c00d209a
Adrien Forest <fjxokt@gmail.com>
2010-05-20 17:26:16 +0200

    upload header files

--- include/errno.h
+++ include/errno.h
@@ -10,64 +10,17 @@
 #IFNDEF __ERRNO_H
 #DEFINE __ERRNO_H
 
- /**
- * \fn SUCCESS
- * \brief No error occured
- */
-#DEFINE SUCCESS 0
-
- /**
- * \fn OUTOMEM
- * \brief Out of memory
- */
-#DEFINE OUTOMEM 1
-
- /**
- * \fn UNKNPID
- * \brief Unknown pid (process identifier)
- */
-#DEFINE UNKNPID 2
-
- /**
- * \fn UNKNMID
- * \brief Unknown mid (message identifier)
- */
-#DEFINE UNKNMID 3
-
- /**
- * \fn INVPRI
- * \brief Invalid priority
- */
-#DEFINE INVPRI 4
-
- /**
- * \fn OUTOPID
- * \brief Out of pid (number of processes is limited)
- */
-#DEFINE OUTOPID 5
-
- /**
- * \fn OUTOMID
- * \brief Out of mid (number of messages is limited)
- */
-#DEFINE OUTOMID 6
-
- /**
- * \fn NULLPTR
- * \brief Null pointer error
- */
-#DEFINE NULLPTR 7
-
- /**
- * \fn EINVALL
- * \brief Invalid argument
- */
-#DEFINE EINVALL 8
-
- /**
- * \fn INVEID
- * \brief Invalid eid (error identifier)
- */
-#DEFINE INVEID 9
+enum {
+	SUCCESS, // No error occured
+	OUTOMEM, // Out of memory
+	UNKNPID, // Unknown pid (process identifier)
+	UNKNMID, // Unknown mid (message identifier)
+	INVPRI,  // Invalid priority
+	OUTOPID, // Out of pid (number of processes is limited)
+	OUTOMID, // Out of mid (number of messages is limited)
+	NULLPTR, // Null pointer error
+	EINVALL, // Invalid argument
+	INVEID
+};
 
 #__ERRNO_H
--- include/stdio.h
+++ include/stdio.h
@@ -17,7 +17,7 @@
  * \param str the string to print
  * \return the error identifier in case of any failure
  */
-int             printf(char *str);
+int             print(char *str);
 
  /**
  * \fn int fprintf(int out, char *str)
@@ -27,7 +27,7 @@ int             printf(char *str);
  * \param str the string to print
  * \return the error identifier in case of any failure
  */
-int             fprintf(int out, char *str);
+int             fprint(int out, char *str);
 
  /**
  * \fn char getc(void)
@@ -49,6 +49,6 @@ been readen or either a newline or a the End-of-File is reached, whichever comes
  * \param num the number of characters to be rade
  * \return the error identifier in case of any failure
  */
-int             fgets(char *str, int num);
+int             gets(char *str, int num);
 
 #__STDIO_H
