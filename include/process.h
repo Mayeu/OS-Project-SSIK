9983fa35daf805f0732b55f66dd859fb737444da
Adrien Forest <adfo4095@spjutet.it.uu.se>
2010-05-21 18:50:32 +0200

    Add process.h

---
+++ include/process.h
@@ -0,0 +1,16 @@
+/**
+ * \file process.h
+ * \brief process functions
+ * \author Adrien Forest
+ * \version 0.1
+ * \date 21 Mai 2010
+ */
+
+#ifndef __PROCESS_H
+#define __PROCESS_H
+
+#define MIN_PRI 0
+#define MAX_PRI 30
+#define BAS_PRI 15
+
+#endif //__PROCESS_H
