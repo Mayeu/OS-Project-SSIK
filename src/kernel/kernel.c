440f3aab464e0fefba038f67e2ed1101d2b2e100
Christophe Carasco <carasco.christophe@gmail.com>
2010-05-22 01:14:05 +0200

    Remove kexception function. Move it to kexception file.

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -43,24 +43,3 @@ kinit()
   /* Forever do nothing. */
   while (1);
 }
-
-
-/* kexception:
- *   Application-specific exception handler, called after registers
- *   have been saved.
- */
-void
-kexception()
-{
-  cause_reg_t     cause;
-
-  /* Make sure that we are here because of a timer interrupt. */
-  cause.reg = kget_cause();
-  kdebug_assert(cause.field.exc == 0);  /* External interrupt */
-  kdebug_assert(cause.field.ip & 0x80); /* Timer interrupt */
-
-  /* Reload timer for another 100 ms (simulated time) */
-  kload_timer(100 * timer_msec);
-
-  /* Icrease the number on the Malta display. */
-} /**/
