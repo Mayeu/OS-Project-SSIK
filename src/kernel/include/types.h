83b1c7e282305086f53acf0c94d3b1a54da06005
Matthieu Maury <mayeu.tik@gmail.com>
2010-05-21 03:43:23 +0200

    Move bool def to stdlib
    Remove all other definition

--- src/kernel/include/types.h
+++ src/kernel/include/types.h
@@ -12,18 +12,12 @@ typedef signed char int8_t;
 typedef signed short int16_t;
 typedef signed int int32_t;
 
+#ifndef __NULL_TYPES_
+#define __NULL_TYPES_
 
-#ifndef __BOOL_TYPES_
-#define __BOOL_TYPES_
+#define NULL ((void *)0)
 
-typedef enum
-{
-  FALSE,
-  TRUE
-} bool;
-
-#endif /* __BOOL_TYPES_  */
-
+#endif /* __NULL_TYPES_ */
 
 #endif /*  */
 
