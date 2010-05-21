3d79e06703b4952d4cd3c2622e1a4d5fc33f03b4
Christophe Carasco <carasco.christophe@gmail.com>
2010-05-22 01:19:19 +0200

    Rename search to search_prgm.

---
+++ src/kernel/kprogram.c
@@ -0,0 +1,25 @@
+
+#include "kprogram.h"
+
+/**
+ * @brief Search a program into the program list
+ * @param *name a pointer to a string, wich represent a possible
+ * programm name
+ * @return: a pointer to the program, or NULL if not found
+ */
+prgm           *
+search_prgm(char *name)
+{
+  return NULL;
+}
+
+/**
+ * @brief Print all the program with description
+ * @param void
+ * @return void
+ */
+void
+print_programs(void)
+{
+
+}
