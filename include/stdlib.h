cd10a75082eb83fe2186b53a94f06ef0e08394ab
Matthieu Maury <mayeu.tik@gmail.com>
2010-05-21 03:46:25 +0200

    Move all bool def in stdlib

--- include/stdlib.h
+++ include/stdlib.h
@@ -1,4 +1,3 @@
-
 #ifndef __NULL_TYPES_
 #define __NULL_TYPES_
 
@@ -6,3 +5,13 @@
 
 #endif /* __NULL_TYPES_ */
 
+#ifndef __BOOL_TYPES_
+#define __BOOL_TYPES_
+
+typedef enum
+{
+  FALSE,
+  TRUE
+} bool;
+
+#endif /* __BOOL_TYPES_  */
