e8f23a2588fa6fb48353e2d2b46fdb53d4b8ab4d
Matthieu Maury <mayeu.tik@gmail.com>
2010-05-22 23:41:14 +0200

    Add one line in the splash screen

--- src/kernel/splash.c
+++ src/kernel/splash.c
@@ -20,6 +20,7 @@ splash(void)
   kprintln("|__     |__     |_|   |_|     < ");
   kprintln("|_______|_______|_______|__|\\__|");
   kprintln("The Simply and Stupidly Implemented Kernel");
+  kprintln("Proudly procrastinated by Adrien, Chris & Mayeu");
 
   kprintln("\nFirst time here? Type help!");
 }
