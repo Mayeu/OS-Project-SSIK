a57b987d5bf64365b538167e54eb8f5686cfc4d7
Matthieu Maury <mayeu.tik@gmail.com>
2010-05-22 12:47:00 +0200

    Add the function in uart.h
    Add the fifo buffer manipulation in uart.h
    Implement the reset_fifo_buffer in uart.c

--- src/kernel/uart.c
+++ src/kernel/uart.c
@@ -3,6 +3,52 @@
  * @brief implemet the uart fuctions
  */
 
+#include "uart.h"
+
+/*
+ * Global variable
+ */
+
+static fifo_buffer uart_fifo;
+
+/*
+ * Fifo functions
+ */
+
+/**
+ * @brief reset the fifo buffer to default value
+ * @param void
+ * @return void
+ */
+void reset_fifo_buffer(void)
+{
+	uart_fifo.start = 0;
+	uart_fifo.end = 0;
+	uart_fifo.length = 0;
+}
+
+/**
+ * @brief push a char in the fifo buffer
+ * @param the char to push
+ * @return OMGROXX if there isan empty space OUTOMEM otherwise
+ */
+uint32_t push_fifo_buffer(char c)
+{
+}
+
+/**
+ * @brief pop a char from the fifo buffer
+ * @parama a pointer to the char to pop
+ * @return OMGROXX if the buffer is not empty, FAILNOOB otherwise
+ */
+uint32_t pop_fifo_buffer(char *c)
+{
+}
+
+/*
+ * UART management functions
+ */
+
 /**
  * @brief initialize the uart
  * @param void
--- src/kernel/uart.h
+++ src/kernel/uart.h
@@ -6,6 +6,9 @@
 #ifndef __UART_H
 #define __UART_H
 
+#include <types.h>
+#include <errno.h>
+
 /**
  * Define
  */
@@ -24,10 +27,41 @@ typedef struct
 {
   char            buffer[FIFO_SIZE];
   uint32_t        length;
-} fifo;
+  uint32_t start;
+  uint32_t end;
+} fifo_buffer;
+
+/*
+ * Functions
+ */
 
 /**
- * Functions
+ * @brief reset the fifo buffer to default value
+ * @param void
+ * @return void
+ */
+void reset_fifo_buffer(void);
+
+/**
+ * @brief push a char in the fifo buffer
+ * @param the char to push
+ * @return OMGROXX if there isan empty space OUTOMEM otherwise
+ */
+uint32_t push_fifo_buffer(char c);
+
+/**
+ * @brief pop a char from the fifo buffer
+ * @parama a pointer to the char to pop
+ * @return OMGROXX if the buffer is not empty, FAILNOOB otherwise
+ */
+uint32_t pop_fifo_buffer(char *c);
+
+/*
+ * bounded fifo function
+ */
+
+/*
+ * UART functions
  */
 
 /**
