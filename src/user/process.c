1a30185ef1fd952a4faacaea6161c2970fa0bacd
Adrien Forest <adfo4095@yxan.it.uu.se>
2010-05-23 22:54:49 +0200

    Add user programs increment.c and fibonacci.c

--- src/user/process.c
+++ src/user/process.c
@@ -13,27 +13,29 @@
  /**
  * Kill the current process.
  */
-int exit()
+int
+exit()
 {
-	return syscall_none(EXIT);
+  return syscall_none(EXIT);
 }
 
  /**
  * Kill the process with the right pid.
  */
-int kill(int pid)
+int
+kill(int pid)
 {
-	return syscall_one(pid, KILL);
+  return syscall_one(pid, KILL);
 }
 
  /**
  * Sleep makes the current process sleep until ’time’ milliseconds seconds have elapsed.
  */
-void sleep(int time)
+void
+sleep(int time)
 {
-	syscall_one(time, SLEEP);
+  syscall_one(time, SLEEP);
 }
-
 
  /**
  * Creates a child process that differs from the parent process only in its PID and
@@ -41,9 +43,26 @@ PPID. If success, the PID of the child process is returned in the parent’s thr
 and a 0 is returned in the child’s thread of execution.
  */
 int
-fourchette(char *name, char* argv[])
+fourchette(char *name, char *argv[])
 {
-	int pid;
-  pid = syscall_two((int32_t)name, (int32_t)argv, FOURCHETTE);
-  return pid;
+  return syscall_two((int32_t) name, (int32_t) argv, FOURCHETTE);
+}
+
+ /**
+ * \brief fill the pcb_info structure given in parameter with the pcb information. Only
+not critical information is given to the user.
+ */
+int
+get_proc_info(int pid, pcbinfo * res)
+{
+  return syscall_two(pid, (int32_t) res, GETPINFO);
+}
+
+ /**
+ * \brief changes the priority of the process from the old one to the new priority ’prio’.
+ */
+int
+chgpri(int pid, int newprio)
+{
+  return syscall_two(pid, newprio, CHGPPRI);
 }
