92c37fe6db8ec0cfd1f5ca15d691c5e44566fb48
Adrien Forest <adfo4095@spjutet.it.uu.se>
2010-05-23 02:54:54 +0200

    Init process.c

---
+++ src/user/process.c
@@ -0,0 +1,15 @@
+/**
+ * \file process.c
+ * \brief process functions
+ * \author Adrien Forest
+ * \version 0.1
+ * \date 21 Mai 2010
+ */
+
+#include <process.h>
+
+int
+fourchette(char name, int prio, int argc, *char[]argv)
+{
+  return 0;
+}
