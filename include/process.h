181451b8092d127c3c1d4ee5383c0b6998ef1413
Christophe Carasco <carasco.christophe@gmail.com>
2010-05-23 18:30:27 +0200

    Move pcbinfo from kprocess.h to process.h.

--- include/process.h
+++ include/process.h
@@ -12,6 +12,29 @@
 #define MIN_PRI 0
 #define MAX_PRI 30
 #define BAS_PRI 15
+#define MAX_ARG 4
+#define ARG_SIZE 20
+#define NSUPERVISED 40
+
+#include <stdlib.h>
+
+/**
+ * \struct pcbinfo
+ * \brief Copy of a process pcb.
+ *
+ * A copy has to be created and passed to make sure that the real pcb will not
+ * be affected in any way.
+ */
+typedef struct
+{
+  int       pid;          /*!< Process identifier. */
+  char            name[ARG_SIZE];       /*!< Process name. */
+  int        pri;          /*!< Process priority. */
+  int        supervised[NSUPERVISED];      /*!< List of supervised processes. */
+  int        supervisor;   /*!< List of supervisor processes. */
+  int        wait;
+  bool            empty;
+} pcbinfo;
 
 int             kill(int pid);
 
