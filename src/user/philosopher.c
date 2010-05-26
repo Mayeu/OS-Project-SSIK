1ce62bb0bc930737350a5b7ae54a38cade59d762
Adrien Forest <adfo4095@yxan.it.uu.se>
2010-05-26 03:17:22 +0200

    Add philosopher.c user program (not finished) + Makefile

---
+++ src/user/philosopher.c
@@ -0,0 +1,20 @@
+/**
+ * \file philosopher.c
+ * \brief blabla
+ * \author Christophe Carasco
+ * \version 0.1
+ * \date 25 mai 2010
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <message.h>
+#include <process.h>
+
+// params: 
+int philosopher(int argc, char *argv[])
+{
+	
+}
+
