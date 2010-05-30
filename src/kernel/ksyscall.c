438e6087f4aa79b0bf0ad2220ed6520a15f7633e
Adrien Forest <adfo4095@yxan.it.uu.se>
2010-05-30 21:55:21 +0200

    Add function calls in ksyscall and modify/add some function names

--- src/kernel/ksyscall.c
+++ src/kernel/ksyscall.c
@@ -55,19 +55,13 @@ syscall_handler(registers_t * regs)
     res = go_to_sleep(regs->a_reg[0]);
     break;
   case BLOCK:
+    res = kblock(regs->a_reg[0]);
     break;
-  case WAKEUP:
+  case UNBLOCK:
+    res = kwakeup(regs->a_reg[0]);
     break;
   case WAIT:
-    /*
-       wait()
-       p = search_pcb(regs->a_reg[0], &pterminate);
-       // copy de la valeur de retour du proc
-       // qu'on attendait
-       *regs->a_reg[1] = p->registers->v_reg[0]; 
-
-       rm(p)
-     */
+    res = waitfor(regs->a_reg[0]);
     break;
   case SEND:
     //mres = (msg_arg *) regs->a_reg[0];
@@ -93,61 +87,30 @@ syscall_handler(registers_t * regs)
     kserror(regs->a_reg[0]);
     break;
   case GETPINFO:
-    // TODO : get_pinfo() TAKES A PID INSTEAD OF A PCB
-    //res = get_pinfo(regs->a_reg[0], (pcbinfo *) regs->a_reg[1]);
-    break;
-  case GETPRI:
-    // TODO : int kgetpri(int pid)
-    //res = kgetpri(regs->a_reg[0]);
+    res = get_pinfo(regs->a_reg[0], (pcbinfo *) regs->a_reg[1]);
     break;
   case GETPID:
     res = pcb_get_pid(get_current_pcb());
     break;
-  case GETPS:
-    {
-/*
-		pls_item* p;
-		char **pnames = (char**)regs->a_reg[0];
-		int *pid = (int*)regs->a_reg[1];
-		int i = 0;
-
-		for (p = plsready.start; p != plsready.end; p = p->next, i++)
-		{
-			strcpy(pcb_get_name(p), get_arg(pnames, i));
-			pid[i] = pcb_get_pid(p);
-		}
-		res = i;
-*/
-      res = -1;
-      break;
-    }
+  case GETALLPID:
+    res = get_all_pid((int *) regs->a_reg[0]);
+    break;
   case CHGPPRI:
-    // TODO : chg_ppri() TAKES A PID INSTEAD OF A PCB
-    //res = chg_ppri(regs->a_reg[0], regs->a_reg[1]);
+    res = chg_ppri(regs->a_reg[0], regs->a_reg[1]);
     break;
   case KILL:
-    // TODO : rm_p() TAKES A PID INSTEAD OF A PCB
-    //res = kkill(regs->a_reg[0]);
+    res = kkill(regs->a_reg[0]);
     break;
   case EXIT:
     // NOTIFY THE SUPERVISOR OF THE EXITING PROCESS !
     //kprint("proc exiting with value "); kprint(itos(regs->a_reg[0], buf));kprintn();
-
-    // TODO : int kexit()
-    //res = kexit()
-/*
-    p = search_pcb(prunning.current->pid, &prunning);
-		p->registers.v_reg[0] = regs->a_reg[0];
-		move(prunning.current->pid, &prunning, &pterminate);
-    schedule();
-*/
+    kexit(regs->a_reg[0]);
     break;
   default:
     kprintln("ERROR: Unknown syscall");
     break;
   }
 
-  //kdebug_println("syscal out");
   // saves the return code
   regs->v_reg[0] = res;
 
